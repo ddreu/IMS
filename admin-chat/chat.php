cea934c2aa5b4e1389361a809985d94843405259
Andrew <3922017@holycross.edu.ph>
2025-04-26 05:38:55 +0800

    update for chat

--- admin-chat/chat.php
+++ admin-chat/chat.php
@@ -34,6 +34,8 @@ $image_path = (!empty($row['image']) && file_exists("../uploads/users/" . $row['
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <link rel="stylesheet" href="css/chat.css">
     <link rel="stylesheet" href="../super_admin/sastyles.css">
     <style>
